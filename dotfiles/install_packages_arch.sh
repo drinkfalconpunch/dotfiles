5d1d1a4c4cbcfa6423b388e0c56309ec5abd373b
John Hudson <drinkfalconpunch@gmail.com>
2018-05-31 13:57:44 -0500

    fixed noconfirm in arch install script

--- dotfiles/install_packages_arch.sh
+++ dotfiles/install_packages_arch.sh
@@ -1,22 +1,22 @@
 log_file=~/install_process_log.txt
 
-sudo pacman -S --no-confirm zsh
+sudo pacman -S --noconfirm zsh
 if type -p zsh > /dev/null; then
 	echo "zsh installed" >> $log_file
 else
 	echo "zsh failed to install" >> $log_file
 fi
 
-sudo pacman -S --no-confirm zsh-syntax-highlighting
+sudo pacman -S --noconfirm zsh-syntax-highlighting
 
-sudo pacman -S --no-confirm tmux
+sudo pacman -S --noconfirm tmux
 if type -p tmux > /dev/null; then
 	echo "tmux installed" >> $log_file
 else
 	echo "tmux failed to install" >> $log_file
 fi
 
-sudo pacman -S --no-confirm python-dev
+sudo pacman -S --noconfirm python-dev
 
 echo "\n--Summary--\n"
 cat $log_file
